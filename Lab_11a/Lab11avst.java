0ebd6e6e125f9b92f5f361cd87d8b462cb14d1f8
15608 <zachary.geraghty@vtsd.com>
2025-06-09 13:11:46 -0400

    Lab 11a

---
+++ Lab_11a/Lab11avst.java
@@ -0,0 +1,42 @@
+package Lab_11a;
+
+    // Lab11avst.java
+// The "Sieve of Eratosthenes" Program
+// This is the student, starting version of the Lab11a assignment.
+
+
+import java.util.Scanner;
+
+
+public class Lab11avst {
+
+
+    public static void main(String[] args) {
+        // This main method needs additions for the 100 point version.
+        Scanner input = new Scanner(System.in);
+        final int MAX = 100;
+        boolean primes[];
+        primes = new boolean[MAX];
+        computePrimes(primes);
+        displayPrimes(primes);
+    }
+
+
+    public static void computePrimes(boolean primes[]) {
+        System.out.println("\nCOMPUTING PRIME NUMBERS");
+        for (int i = 2; i < primes.length; i++)
+
+            primes[i] = true;
+    }
+
+    public static void displayPrimes(boolean primes[]) {
+        System.out.println("\n\nPRIMES BETWEEN 1 AND " + primes.length);
+        //output all list value
+        for (int i = 2; i < primes.length; i++) {
+            if (primes[i] == true) {
+                System.out.print(primes[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+}
