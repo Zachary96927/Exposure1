75a70fedcba70e987855b8025fcee93e68e899fd
15608 <zachary.geraghty@vtsd.com>
2025-06-10 12:22:56 -0400

    Lab 11a finished

--- Lab_11a/Lab11avst.java
+++ Lab_11a/Lab11avst.java
@@ -11,22 +11,34 @@ import java.util.Scanner;
 public class Lab11avst {
 
 
-    public static void main(String[] args) {
-        // This main method needs additions for the 100 point version.
+    public static void main(String[] args)
+    {
         Scanner input = new Scanner(System.in);
-        final int MAX = 100;
-        boolean primes[];
-        primes = new boolean[MAX];
+        System.out.print("Enter the primes upper bound ===>> ");
+        final int MAX = input.nextInt();
+        boolean primes[] = new boolean[MAX];
         computePrimes(primes);
         displayPrimes(primes);
     }
 
-
     public static void computePrimes(boolean primes[]) {
         System.out.println("\nCOMPUTING PRIME NUMBERS");
         for (int i = 2; i < primes.length; i++)
-
             primes[i] = true;
+
+
+        for (int i = 2; i < primes.length; i++)
+            if (primes[i] == true){
+                for (int j = i + i; j < primes.length; j = j + i)
+                    primes[j] = false;
+            }
+
+
+
+
+
+
+
     }
 
     public static void displayPrimes(boolean primes[]) {
